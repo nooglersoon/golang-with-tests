978a033ed48ad565dded77a081830a9ce60639a9
nooglersoon <nooglersoon@outlook.co.id>
2023-07-04 08:03:11 +0700

    move chapter 1

---
+++ Chapter 1 - Hello World/hello.go
@@ -0,0 +1,67 @@
+// Packages are ways of grouping up related Go code together.
+package main
+
+// With import "fmt" we are importing a package which contains the Println function that we use to print.
+import "fmt"
+
+/*
+The main function will be executed after all the initialization tasks have been performed.
+In this function, you usually call other packages and implement your program logic.
+*/
+func init() {
+	fmt.Println("launch initialization")
+}
+
+func main() {
+	fmt.Println(createHello("Aji", "spanish"))
+}
+
+/*
+
+The function name starts with a lowercase letter.
+In Go, public functions start with a capital letter and private ones start with a lowercase.
+We don't want the internals of our algorithm to be exposed to the world, so we made this function private.
+
+*/
+
+// Is good to seperate domain code from the outside world (side-effects)
+// Separate these conserns so its easier to test them
+// Followed string is to tell the compiler that the followed function return a string data type
+func createHello(name string, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+	// Declare prefix
+	prefix := greetingPrefix(language)
+	return prefix + name + "!"
+}
+
+// In our function signature we have made a named return value (prefix string).
+// This will create a variable called prefix in your function.
+// You can return whatever it's set to by just calling return rather than return prefix.
+func greetingPrefix(name string) (prefix string) {
+	switch name {
+	case "english":
+		prefix = englishHelloPrefix
+	case "spanish":
+		prefix = spanishHelloPrefix
+	case "french":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+// Defining a constants
+// const englishHelloPrefix = "Hello "
+// const spanishHelloPrefix = "Hola "
+// const frenchHelloPrefix = "Bonjour "
+
+// We can group constants in a block instead of declaring them each on their own line
+const (
+	englishHelloPrefix = "Hello "
+	spanishHelloPrefix = "Hola "
+	frenchHelloPrefix  = "Bonjour "
+)
---
+++ Chapter 1 - Hello World/hello_test.go
@@ -0,0 +1,59 @@
+package main
+
+import "testing"
+
+// We can run multiple tests at once by grouping into subtests.
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := createHello("Chris", "")
+		want := "Hello Chris!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := createHello("", "")
+		want := "Hello World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := createHello("Ajie", "spanish")
+		want := "Hola Ajie!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := createHello("Ajie", "french")
+		want := "Bonjour Ajie!"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+// Refactor error message by extracting into a helper
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper. By doing this when it fails the line number reported will be in our function call rather than inside our test helper. This will help other developers track down problems easier.
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+/*
+
+Basic Tests
+
+func TestHello(t *testing.T) {
+	// Declare variable
+	// varName := value
+	got := createHello("Fauzi")
+	want := "Hello Fauzi!"
+
+	if got != want {
+		// We are calling the Errorf method on our t which will print out a message and fail the test.
+		// The f stands for format which allows us to build a string with values inserted into the placeholder values %q.
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+*/
